ed96cbc53237bfd8d3ec03107710bcdf2f5067b1
Christian Ramirez <christian.r0602@gmail.com>
2022-03-18 10:33:21 -0500

    created classes for Ninja and Pet

---
+++ pet.py
@@ -0,0 +1,10 @@
+class Pet():
+    def __init__(self, name, type, tricks):
+      self.name = name
+      self.type = type
+      self.tricks = tricks
+    
+# implement the following methods:
+# walk() - walks the ninja's pet invoking the pet play() method
+# feed() - feeds the ninja's pet invoking the pet eat() method
+# bathe() - cleans the ninja's pet invoking the pet noise() method
